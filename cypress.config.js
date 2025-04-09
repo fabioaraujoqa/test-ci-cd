647c59ec7e28a83d61eab2c86c7b2172c9a21587
fabioaraujoqa <fabio.araujo.qa@gmail.com>
2025-04-09 13:37:36 -0300

    alteração na confi 2g

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.name === 'chrome') {
-          launchOptions.args.push('--lang=en-US');
-        }
-        return launchOptions;
-      });
+      // implement node event listeners here
     },
     baseUrl: 'https://qa-impact.github.io/'
   },
